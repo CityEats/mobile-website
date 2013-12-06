9500e5c309526e67001fe3dfa4e138df85e88245
max777k <max777k@gmail.com>
2013-12-06 16:05:00 +0200

    optimized JS

---
+++ scripts/build.js
@@ -0,0 +1,33 @@
+({baseUrl: ".",
+    paths: {
+		backbone: 'vendors/backbone.marionette/backbone',
+        underscore: 'vendors/backbone.marionette/underscore',
+        jquery: 'vendors/backbone.marionette/jquery',
+        marionette: 'vendors/backbone.marionette/core/amd/backbone.marionette',
+        'backbone.wreqr': 'vendors/backbone.marionette/core/amd/backbone.wreqr',
+        'backbone.eventbinder': 'vendors/backbone.marionette/core/amd/backbone.eventbinder',
+        'backbone.babysitter': 'vendors/backbone.marionette/core/amd/backbone.babysitter',
+        text: 'vendors/require/text',        
+        basicItemView: 'views/shared/basicItemView',
+        rateIt: 'vendors/plugins/rateit/jquery.rateit.min'
+    },
+	shim: {
+        jquery: {
+            exports: 'jQuery'
+        },
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        rateIt: {
+            deps: ['jquery'],
+            exports: 'rateIt'
+        }
+    },
+	//optimize: "none",
+    name: "main",
+    out: "main-built.js"
+})
